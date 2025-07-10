ec11164466b2a186921306f568a609fdb4bff57d
FernandoH <fernando.huilca@epn.edu.ec>
2025-07-09 22:02:56 -0500

    Division en modulos guia inicial commit #1

---
+++ src/EjerciciosIteractivos_Modulo/Leccion.java
@@ -0,0 +1,12 @@
+package EjerciciosIteractivos_Modulo;
+
+public class Leccion {
+    //Atributos que sugerimos tenga una lección
+    private String nombre; //De que va la lección en general?
+
+
+
+    public String getNombre() {
+        return nombre;
+    }
+}
