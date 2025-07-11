7b2dcfeebc78628a845e4785010501ac425e0d89
Erick Medardo <erick.alpusig@epn.edu.ec>
2025-07-11 11:24:24 -0500

    Creando package Comunidad_Modulo

---
+++ src/Comunidad_Modulo/Comunidad.java
@@ -0,0 +1,37 @@
+package Comunidad_Modulo;
+
+public class Comunidad {
+    private String nombre;
+    private String descripcion;
+    private int numeroMiembros;
+
+    public Comunidad(String nombre, String descripcion, int numeroMiembros) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.numeroMiembros = numeroMiembros;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getNumeroMiembros() {
+        return numeroMiembros;
+    }
+
+    public void setNumeroMiembros(int numeroMiembros) {
+        this.numeroMiembros = numeroMiembros;
+    }
+}
